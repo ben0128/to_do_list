1eb3dfa6b62c2b130e7c896d34ff823779c5105f
Banwind <a84012807@gmail.com>
2023-02-20 00:06:08 +0800

    feat: add todo seeder

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
 
-app.get('/',(req,res)=>{
-  res.send('index')
+const app = express()
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i=0; i<10; i++) {
+    Todo.create({name:`name-${i}`})
+  }
+  
+  console.log('done')
 })
 
-app.listen(port, ()=>{
+app.get('/',(req,res)=>{
+  res.send('Hello world')
+})
+
+app.listen(3000, ()=>{
   console.log('App is running on http://localhost:3000')
 })
