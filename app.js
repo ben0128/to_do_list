7743d1c305aec4f26e2fdcc2c5bc8f0a9647ca56
Banwind <a84012807@gmail.com>
2023-02-22 18:42:30 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
+const Todo = require('./models/todo')
 
 const app = express()
 
@@ -32,7 +33,10 @@ db.once('open', () => {
 })
 
 app.get('/',(req, res) => {
-  res.render('index')
+  Todo.find()  // 把所有資料拿出來，也可以在括號加上想查詢的資料
+    .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos }))  // 將資料傳給 index 樣板
+    .catch(error => console.error('error')) // 錯誤處理
 })
 
 app.listen(3000, ()=>{
