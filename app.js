a0b917503dcaa3c9e2d9bb65c8466b2bbd21e62d
Banwind <a84012807@gmail.com>
2023-03-16 16:42:42 +0800

    feat: add user registration feature

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
-const bodyParser = require('body-parser') // 引用 body-parser
 const app = express()
 const methodOverride = require('method-override') // 載入 method-override
 const routes = require('./routes') // 將 request 導入路由器
@@ -15,7 +14,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 
-app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(express.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(routes)
 
