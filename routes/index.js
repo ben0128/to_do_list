e044b7a8f7978208b652ec5c440e7ffba99a60c9
Banwind <a84012807@gmail.com>
2023-03-16 14:56:18 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos') // 引入 todos 模組程式碼
+const users = require('./modules/users')
 
+router.use('/users', users)
+router.use('/todos', todos)
 router.use('/', home)
 
-router.use('/todos', todos) // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
+// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 
 // 準備引入路由模組
 // 匯出路由器
