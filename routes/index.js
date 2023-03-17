d633d520b697c33f52c27b386e18d7464d0204ac
Banwind <a84012807@gmail.com>
2023-03-17 15:00:19 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,9 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos') // 引入 todos 模組程式碼
 const users = require('./modules/users')
-
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 router.use('/users', users)
-router.use('/todos', todos)
-router.use('/', home)
-
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-
-// 準備引入路由模組
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
 // 匯出路由器
 module.exports = router
